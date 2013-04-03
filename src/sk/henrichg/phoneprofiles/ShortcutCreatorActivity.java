c24ac11f108304c5abffacd22c58141bd6856ac1
henrichg <henrich.gron@chello.sk>
2013-04-03 19:39:45 +0200

    Shortcut into profile - create shortcut

--- src/sk/henrichg/phoneprofiles/ShortcutCreatorActivity.java
+++ src/sk/henrichg/phoneprofiles/ShortcutCreatorActivity.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import com.actionbarsherlock.app.SherlockActivity;
 
+import android.content.Intent;
+import android.content.Intent.ShortcutIconResource;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -39,12 +41,34 @@ public class ShortcutCreatorActivity extends SherlockActivity {
 
 				Log.d("ShortcutCreatorActivity.onItemClick", "xxxx");
 				
-				//activateProfileWithAlert(position);
+				createShortcut(position);
 
 			}
 			
 		});
 		
+	}
+	
+	private void createShortcut(int position)
+	{
+		Profile profile = profileList.get(position);
+		
+		Intent shortcutIntent = new Intent(this, PhoneProfilesActivity.class);
+		
+		// tu musime spravit nacitanie ikony z profilu + bacha na ikony z sd karty
+		ShortcutIconResource iconResource = ShortcutIconResource.fromContext(this, R.drawable.ic_profile_default);
+		
+		Intent intent = new Intent();
+		intent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent);
+		intent.putExtra(Intent.EXTRA_SHORTCUT_NAME, profile.getName());
+		intent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, iconResource);
+		
+		// PhoneProfilesActivity musi toto testovat, a len spravit aktivaciu profilu
+		intent.putExtra("shortcut_profile_id", profile.getID());
+		
+		setResult(RESULT_OK, intent);
+		
+		finish();
 	}
 
 }
