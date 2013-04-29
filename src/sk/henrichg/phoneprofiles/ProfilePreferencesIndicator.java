bbfc0e46f9498332af44dc3038a355233bc4040d
henrichg <henrich.gron@chello.sk>
2013-04-29 15:03:45 +0200

    Mobile data on/off - final

--- src/sk/henrichg/phoneprofiles/ProfilePreferencesIndicator.java
+++ src/sk/henrichg/phoneprofiles/ProfilePreferencesIndicator.java
@@ -56,6 +56,11 @@ public class ProfilePreferencesIndicator {
 				parent.addView(createIndicator(R.drawable.ic_profile_pref_airplane_mode, parent.getContext()));				
 			if (profile.getDeviceAirplaneMode() == 2)
 				parent.addView(createIndicator(R.drawable.ic_profile_pref_airplane_mode_off, parent.getContext()));				
+			// mobile data
+			if ((profile.getDeviceMobileData() == 1) || (profile.getDeviceMobileData() == 3))
+				parent.addView(createIndicator(R.drawable.ic_profile_pref_mobiledata, parent.getContext()));				
+			if (profile.getDeviceAirplaneMode() == 2)
+				parent.addView(createIndicator(R.drawable.ic_profile_pref_mobiledata_off, parent.getContext()));				
 			// wifi
 			if ((profile.getDeviceWiFi() == 1) || (profile.getDeviceWiFi() == 3))
 				parent.addView(createIndicator(R.drawable.ic_profile_pref_wifi, parent.getContext()));				
