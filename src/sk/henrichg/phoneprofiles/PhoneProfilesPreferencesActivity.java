85d494022dc77ce6cc85c2006a2e7ab5351fc5aa
henrichg <henrich.gron@chello.sk>
2013-06-13 16:58:01 +0200

    Tablet UI - step 1

--- src/sk/henrichg/phoneprofiles/PhoneProfilesPreferencesActivity.java
+++ src/sk/henrichg/phoneprofiles/PhoneProfilesPreferencesActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 //import android.content.Context;
 //import android.content.Intent;
