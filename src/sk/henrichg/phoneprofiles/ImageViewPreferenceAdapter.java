abf81fd114e4e4b84a016257e915a28bf65c58bb
henrichg <henrich.gron@chello.sk>
2013-06-14 17:53:52 +0200

    Using added profile icons

--- src/sk/henrichg/phoneprofiles/ImageViewPreferenceAdapter.java
+++ src/sk/henrichg/phoneprofiles/ImageViewPreferenceAdapter.java
@@ -12,10 +12,22 @@ public class ImageViewPreferenceAdapter extends BaseAdapter {
 	private Context context;
 	
 	static final String[] ThumbsIds = {
-		"ic_profile_default", "ic_profile_home", 
-		"ic_profile_outdoors_1", "ic_profile_outdoors_2", "ic_profile_outdoors_3",
-		"ic_profile_meeting", "ic_profile_work_1", "ic_profile_work_2", "ic_profile_work_4",
-		"ic_profile_sleep", "ic_profile_mute"
+		"ic_profile_default", 
+		
+		"ic_profile_home", "ic_profile_home_2", 
+		
+		"ic_profile_outdoors_1", "ic_profile_outdoors_2", "ic_profile_outdoors_3", "ic_profile_outdoors_4",
+		"ic_profile_outdoors_5",
+		 
+		"ic_profile_meeting", "ic_profile_mute", 
+		
+		"ic_profile_work_1", "ic_profile_work_2", "ic_profile_work_3", "ic_profile_work_4", "ic_profile_work_5",
+		"ic_profile_work_6", "ic_profile_work_7", "ic_profile_work_8", "ic_profile_work_9", "ic_profile_work_10",
+		"ic_profile_work_11", "ic_profile_work_12",
+		
+		"ic_profile_sleep", "ic_profile_sleep_2", "ic_profile_night",
+		
+		"ic_profile_car_1", "ic_profile_car_2", "ic_profile_car_3", "ic_profile_car_4"
 	};
 	
 	public ImageViewPreferenceAdapter(Context c)
