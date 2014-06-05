647b4b5cc3c874be45fff8982f98ef1535109d6b
henrisko <henrich.gron@chello.sk>
2014-06-05 21:24:41 +0200

    Removed unused imports

--- src/sk/henrichg/phoneprofilesplus/PackageReplacedReceiver.java
+++ src/sk/henrichg/phoneprofilesplus/PackageReplacedReceiver.java
@@ -1,11 +1,8 @@
 package sk.henrichg.phoneprofilesplus;
 
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.support.v4.app.NotificationCompat;
 
 public class PackageReplacedReceiver extends BroadcastReceiver {
 
