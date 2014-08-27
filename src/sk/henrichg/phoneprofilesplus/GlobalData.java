48676247c1c70978aceac00f8cbbca9f65facdb1
henrisko <henrich.gron@chello.sk>
2014-08-27 22:44:17 +0200

    .

--- src/sk/henrichg/phoneprofilesplus/GlobalData.java
+++ src/sk/henrichg/phoneprofilesplus/GlobalData.java
@@ -412,7 +412,7 @@ public class GlobalData extends Application {
 	    applicationEventWifiEnableWifi = preferences.getBoolean(PREF_APPLICATION_EVENT_WIFI_ENABLE_WIFI, true);
 	    applicationBackgroundProfile = preferences.getString(PREF_APPLICATION_BACKGROUND_PROFILE, "-999");
 	    applicationActivatorGridLayout = preferences.getBoolean(PREF_APPLICATION_ACTIVATOR_GRID_LAYOUT, false);
-	    applicationWidgetListGridLayout = preferences.getBoolean(PREF_APPLICATION_WIDGET_LIST_GRID_LAYOUT, false);;
+	    applicationWidgetListGridLayout = preferences.getBoolean(PREF_APPLICATION_WIDGET_LIST_GRID_LAYOUT, false);
 	}
 	
 	private static String getVolumeLevelString(int percentage, int maxValue)
