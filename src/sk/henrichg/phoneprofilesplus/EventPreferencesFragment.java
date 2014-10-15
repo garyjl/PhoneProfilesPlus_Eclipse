32faa22aadf52113669a4a7f6694d3910868fcea
henrichg <henrich.gron@chello.sk>
2014-10-15 23:24:27 +0200

    .

--- src/sk/henrichg/phoneprofilesplus/EventPreferencesFragment.java
+++ src/sk/henrichg/phoneprofilesplus/EventPreferencesFragment.java
@@ -8,7 +8,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
-import android.preference.CheckBoxPreference;
 import android.preference.PreferenceManager;
 import android.preference.RingtonePreference;
 import android.support.v7.app.ActionBarActivity;
