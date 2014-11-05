594e769af35ce43983d12ebf5db199968a3c667b
henrichg <henrich.gron@chello.sk>
2014-11-05 17:38:39 +0100

    Fixed display flickering and remove focus from active application when brightness is set.

---
+++ src/sk/henrichg/phoneprofilesplus/BrightnessView.java
@@ -0,0 +1,23 @@
+package sk.henrichg.phoneprofilesplus;
+
+import android.content.Context;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+
+public class BrightnessView extends ViewGroup {
+
+	Context context;
+	
+	public BrightnessView(Context context) {
+		super(context);
+		this.context = context;
+	}
+
+	@Override
+	protected void onLayout(boolean changed, int l, int t, int r, int b) {
+		//WindowManager windowManager = (WindowManager)context.getSystemService(Context.WINDOW_SERVICE);
+		//windowManager.removeViewImmediate(this);
+		
+	}
+
+}
