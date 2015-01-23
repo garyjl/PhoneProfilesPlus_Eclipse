1b26897a33d1b70f6f09115e95f9175a75d75d41
henrichg <henrich.gron@chello.sk>
2015-01-23 14:23:04 +0100

    Added new profile icons

--- src/sk/henrichg/phoneprofilesplus/ImageViewPreferenceAdapter.java
+++ src/sk/henrichg/phoneprofilesplus/ImageViewPreferenceAdapter.java
@@ -22,20 +22,20 @@ public class ImageViewPreferenceAdapter extends BaseAdapter {
 		"ic_profile_outdoors_1", "ic_profile_outdoors_2", "ic_profile_outdoors_3", "ic_profile_outdoors_4",
 		"ic_profile_outdoors_5",
 		 
-		"ic_profile_meeting", "ic_profile_meeting_2", "ic_profile_meeting_3", "ic_profile_mute", "ic_profile_mute_2",  
+		"ic_profile_meeting", "ic_profile_meeting_2", "ic_profile_meeting_3", "ic_profile_mute", "ic_profile_mute_2",
+		"ic_profile_volume_1", "ic_profile_volume_2", "ic_profile_volume_3",
 		
 		"ic_profile_work_1", "ic_profile_work_2", "ic_profile_work_3", "ic_profile_work_4", "ic_profile_work_5",
 		"ic_profile_work_6", "ic_profile_work_7", "ic_profile_work_8", "ic_profile_work_9", "ic_profile_work_10",
 		"ic_profile_work_11", "ic_profile_work_12",
 		
 		"ic_profile_sleep", "ic_profile_sleep_2", "ic_profile_night",
+		"ic_profile_call_1",
 		
 		"ic_profile_car_1", "ic_profile_car_2", "ic_profile_car_3", "ic_profile_car_4", "ic_profile_car_5",
 		"ic_profile_car_6", "ic_profile_car_7", "ic_profile_car_8", "ic_profile_car_9",
 		
 		"ic_profile_battery_1", "ic_profile_battery_2", "ic_profile_battery_3",
-		
-		"ic_profile_call_1",
 		
 		"ic_profile_culture_1", "ic_profile_culture_2", "ic_profile_culture_3", "ic_profile_culture_4"
 	};
